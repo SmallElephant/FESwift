7bce61d143e09412af867a7a2505b7ba388e6868
FlyElephant <1061731917@qq.com>
2016-06-05 15:54:39 +0800

    初始化

---
+++ 1-SwiftSynax/1-SwiftSynax/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  1-SwiftSynax
+//
+//  Created by keso on 16/6/5.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ 2-UITableView/2-UITableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2-UITableView
+//
+//  Created by keso on 16/6/5.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
