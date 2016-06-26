c040f4fa18159289d356dc2e5898bf5742711617
FlyElephant <1061731917@qq.com>
2016-06-26 14:10:09 +0800

    update

--- 1-SwiftSynax/1-SwiftSynax/main.swift
+++ 1-SwiftSynax/1-SwiftSynax/main.swift
@@ -10,3 +10,35 @@ import Foundation
 
 print("Hello, World!")
 
+class SurveyQuestion {
+    var text: String
+    var response: String?
+    init(text: String) {
+        self.text = text
+    }
+    func ask() {
+        print(text)
+    }
+}
+let cheeseQuestion = SurveyQuestion(text: "Do you like cheese?")
+cheeseQuestion.ask()
+// 输出 "Do you like cheese?"
+cheeseQuestion.response = "Yes, I do like cheese."
+
+func swapTwoValues<T>(inout a: T, inout _ b: T) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 3
+var anotherInt = 100
+swapTwoValues(&someInt, &anotherInt)
+// someInt is now 107, and anotherInt is now 3
+
+print("First:\(someInt)----Next:\(anotherInt)")
+
+var someString = "hello"
+var anotherString = "world"
+swapTwoValues(&someString, &anotherString)
+print("First:\(someString)----Next:\(anotherString)")
